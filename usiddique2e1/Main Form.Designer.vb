2e6d8ca764af54f09ba58125fab0649756f6e945
us7923 <us7923@UMMER>
2017-10-29 02:44:32 -0500

    Added Exercise 2E1

---
+++ usiddique2e1/Main Form.Designer.vb
@@ -0,0 +1,264 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnCalculateOr = New System.Windows.Forms.Button()
+        Me.btnCalculateAnd = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblShipping = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtSavannah = New System.Windows.Forms.TextBox()
+        Me.txtOrder = New System.Windows.Forms.TextBox()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.txtSales = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lblCommission = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.btnEqual = New System.Windows.Forms.Button()
+        Me.btnNotEqual = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnCalculateOr)
+        Me.GroupBox1.Controls.Add(Me.btnCalculateAnd)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.lblShipping)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.txtSavannah)
+        Me.GroupBox1.Controls.Add(Me.txtOrder)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(22, 25)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(660, 138)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Free shipping for orders over $100 with Savannah CC"
+        '
+        'btnCalculateOr
+        '
+        Me.btnCalculateOr.Location = New System.Drawing.Point(518, 79)
+        Me.btnCalculateOr.Name = "btnCalculateOr"
+        Me.btnCalculateOr.Size = New System.Drawing.Size(119, 38)
+        Me.btnCalculateOr.TabIndex = 7
+        Me.btnCalculateOr.Text = "Calculate (Or)"
+        Me.btnCalculateOr.UseVisualStyleBackColor = True
+        '
+        'btnCalculateAnd
+        '
+        Me.btnCalculateAnd.Location = New System.Drawing.Point(518, 20)
+        Me.btnCalculateAnd.Name = "btnCalculateAnd"
+        Me.btnCalculateAnd.Size = New System.Drawing.Size(119, 38)
+        Me.btnCalculateAnd.TabIndex = 6
+        Me.btnCalculateAnd.Text = "Calculate (And)"
+        Me.btnCalculateAnd.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(342, 30)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(109, 16)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Shipping charge:"
+        '
+        'lblShipping
+        '
+        Me.lblShipping.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblShipping.Location = New System.Drawing.Point(345, 57)
+        Me.lblShipping.Name = "lblShipping"
+        Me.lblShipping.Size = New System.Drawing.Size(100, 30)
+        Me.lblShipping.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(169, 31)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(106, 16)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "&Savannah (Y/N):"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(29, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(92, 16)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "&Order amount:"
+        '
+        'txtSavannah
+        '
+        Me.txtSavannah.Location = New System.Drawing.Point(169, 57)
+        Me.txtSavannah.Name = "txtSavannah"
+        Me.txtSavannah.Size = New System.Drawing.Size(100, 22)
+        Me.txtSavannah.TabIndex = 1
+        '
+        'txtOrder
+        '
+        Me.txtOrder.Location = New System.Drawing.Point(32, 58)
+        Me.txtOrder.Name = "txtOrder"
+        Me.txtOrder.Size = New System.Drawing.Size(100, 22)
+        Me.txtOrder.TabIndex = 0
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(258, 376)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(164, 45)
+        Me.btnExit.TabIndex = 1
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.btnNotEqual)
+        Me.GroupBox2.Controls.Add(Me.btnEqual)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.lblCommission)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.txtSales)
+        Me.GroupBox2.Controls.Add(Me.txtID)
+        Me.GroupBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(22, 215)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(660, 135)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "15% commission for sales > 25000 with codes A1, B2, C3"
+        '
+        'txtID
+        '
+        Me.txtID.Location = New System.Drawing.Point(25, 64)
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(40, 22)
+        Me.txtID.TabIndex = 0
+        '
+        'txtSales
+        '
+        Me.txtSales.Location = New System.Drawing.Point(99, 64)
+        Me.txtSales.Name = "txtSales"
+        Me.txtSales.Size = New System.Drawing.Size(100, 22)
+        Me.txtSales.TabIndex = 1
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(22, 36)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(24, 16)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "ID:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(108, 36)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(46, 16)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "Sales:"
+        '
+        'lblCommission
+        '
+        Me.lblCommission.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblCommission.Location = New System.Drawing.Point(263, 55)
+        Me.lblCommission.Name = "lblCommission"
+        Me.lblCommission.Size = New System.Drawing.Size(101, 31)
+        Me.lblCommission.TabIndex = 4
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(270, 36)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(85, 16)
+        Me.Label7.TabIndex = 5
+        Me.Label7.Text = "Commission:"
+        '
+        'btnEqual
+        '
+        Me.btnEqual.Location = New System.Drawing.Point(490, 21)
+        Me.btnEqual.Name = "btnEqual"
+        Me.btnEqual.Size = New System.Drawing.Size(112, 38)
+        Me.btnEqual.TabIndex = 6
+        Me.btnEqual.Text = "Calculate (=)"
+        Me.btnEqual.UseVisualStyleBackColor = True
+        '
+        'btnNotEqual
+        '
+        Me.btnNotEqual.Location = New System.Drawing.Point(490, 75)
+        Me.btnNotEqual.Name = "btnNotEqual"
+        Me.btnNotEqual.Size = New System.Drawing.Size(112, 38)
+        Me.btnNotEqual.TabIndex = 7
+        Me.btnNotEqual.Text = "Calculate (<>)"
+        Me.btnNotEqual.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(714, 445)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "frmMain"
+        Me.Text = "usiddique2E1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtSavannah As TextBox
+    Friend WithEvents txtOrder As TextBox
+    Friend WithEvents lblShipping As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnCalculateOr As Button
+    Friend WithEvents btnCalculateAnd As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnExit As Button
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtSales As TextBox
+    Friend WithEvents txtID As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents lblCommission As Label
+    Friend WithEvents btnNotEqual As Button
+    Friend WithEvents btnEqual As Button
+End Class
---
+++ usiddique2e1/Main Form.vb
@@ -0,0 +1,102 @@
+﻿'Name: Exercise 2e, AndAlso, OrElse
+'Purpose: Calculate Shipping and commission using AndAlso/OrElse
+'Programmer" Ummer Siddique on 10/29/2017
+
+
+Option Strict On
+Option Explicit On
+Option Infer Off
+
+
+
+Public Class frmMain
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnCalculateAnd_Click(sender As Object, e As EventArgs) Handles btnCalculateAnd.Click
+        Const dblShipping_Charge As Double = 9.0
+        Const dblNo_Shipping_Charge As Double = 0.0
+        Dim dblOrder As Double
+        Dim strSavannah As String
+        strSavannah = txtSavannah.Text
+        Double.TryParse(txtOrder.Text, dblOrder)
+        If strSavannah.ToUpper = "Y" AndAlso dblOrder >= 100 Then
+
+            lblShipping.Text = dblNo_Shipping_Charge.ToString("c2")
+        Else
+
+            lblShipping.Text = dblShipping_Charge.ToString("c2")
+        End If
+
+    End Sub
+
+    Private Sub btnCalculateOr_Click(sender As Object, e As EventArgs) Handles btnCalculateOr.Click
+        Const dblShipping_Charge As Double = 9.0
+        Const dblNo_Shipping_Charge As Double = 0.0
+        Dim dblOrder As Double
+        Dim strSavannah As String
+        strSavannah = txtSavannah.Text
+        Double.TryParse(txtOrder.Text, dblOrder)
+        If strSavannah.ToUpper <> "Y" OrElse dblOrder < 100 Then
+            lblShipping.Text = dblShipping_Charge.ToString("c2")
+
+        Else
+            lblShipping.Text = dblNo_Shipping_Charge.ToString("c2")
+        End If
+
+    End Sub
+
+    Private Sub ClearShippingLabels(sender As Object, e As EventArgs) _
+        Handles txtOrder.TextChanged, txtSavannah.TextChanged
+        lblShipping.Text = String.Empty
+    End Sub
+
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+
+    Private Sub btnEqual_Click(sender As Object, e As EventArgs) Handles btnEqual.Click
+        Dim dblSales As Double
+        Dim dblCommission As Double
+        Dim strId As String
+        strId = txtID.Text.ToUpper
+        Double.TryParse(txtSales.Text, dblSales)
+        If (strId = "A1" OrElse
+           strId = "B2" OrElse
+           strId = "C3") AndAlso dblSales >= 25000 Then
+            dblCommission = dblSales * 0.15
+            lblCommission.Text = dblCommission.ToString("c2")
+
+        Else
+            dblCommission = dblSales * 0.12
+            lblCommission.Text = dblCommission.ToString("c2")
+        End If
+    End Sub
+
+    Private Sub btnNotEqual_Click(sender As Object, e As EventArgs) Handles btnNotEqual.Click
+        Dim dblSales As Double
+        Dim dblCommission As Double
+        Dim strId As String
+        strId = txtID.Text.ToUpper
+        Double.TryParse(txtSales.Text, dblSales)
+        If (strId <> "A1" AndAlso
+           strId <> "B2" AndAlso
+           strId <> "C3") OrElse dblSales < 25000 Then
+            dblCommission = dblSales * 0.12
+            lblCommission.Text = dblCommission.ToString("c2")
+        Else
+
+            dblCommission = dblSales * 0.15
+            lblCommission.Text = dblCommission.ToString("c2")
+        End If
+
+    End Sub
+
+    Private Sub ClearCommissionLabels(sender As Object, e As EventArgs) _
+     Handles txtID.TextChanged, txtSales.TextChanged
+        lblCommission.Text = String.Empty
+    End Sub
+End Class
